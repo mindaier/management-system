b0b4e9a2df4322d2b0b3cddc05b495b733e6df71
抓猫的鱼 <5338768+daiminr@user.noreply.gitee.com>
2020-09-15 17:30:43 +0800

    分页器模块功能完成

--- management-system/src/views/layout/subject/subject.vue
+++ management-system/src/views/layout/subject/subject.vue
@@ -1,15 +1,15 @@
 <template>
-	<div>
-		<el-card class="card">
+	<div class="subject">
+		<el-card>
 			<el-form :model="form" :inline="true">
 				<el-form-item label="学科编号">
-					<el-input v-model="form.rid"></el-input>
+					<el-input v-model="form.rid" class="w100"></el-input>
 				</el-form-item>
 				<el-form-item label="学科名称">
 					<el-input v-model="form.name" class="w149"></el-input>
 				</el-form-item>
 				<el-form-item label="创建者">
-					<el-input v-model="form.username"></el-input>
+					<el-input v-model="form.username" class="w100"></el-input>
 				</el-form-item>
 				<el-form-item label="状态">
 					<el-select v-model="form.status" class="w149">
@@ -18,9 +18,9 @@
 					</el-select>
 				</el-form-item>
 				<el-form-item>
-					<el-button type="primary">搜索</el-button>
-					<el-button>清除</el-button>
-					<el-button type="primary">+新增学科</el-button>
+					<el-button type="primary" class="search">搜索</el-button>
+					<el-button class="clear">清除</el-button>
+					<el-button type="primary" class="add">+新增学科</el-button>
 				</el-form-item>
 			</el-form>
 		</el-card>
@@ -37,12 +37,14 @@
 				<el-table-column label="创建日期" prop="create_time"></el-table-column>
 				<el-table-column label="状态" width="50px">
 					<template v-slot="scope">
-						{{ scope.row.status == 0 ? "禁用" : "启用" }}
+						<span :class="{ red: scope.row.status == 1 }">
+							{{ scope.row.status == 0 ? "禁用" : "启用" }}
+						</span>
 					</template>
 				</el-table-column>
 				<el-table-column label="操作" width="300px">
 					<template slot-scope="scope">
-						<el-button type="text">编辑</el-button>
+						<el-button type="text" @click="edit(scope.row)">编辑</el-button>
 						<el-button type="text">{{
 							scope.row.status == 1 ? "禁用" : "启用"
 						}}</el-button>
@@ -55,7 +57,7 @@
 					@size-change="handleSizeChange"
 					@current-change="handleCurrentChange"
 					:current-page="pagination.currentPage"
-					:page-sizes="[10, 20, 30, 40]"
+					:page-sizes="[1, 2, 3]"
 					:page-size="pagination.pageSize"
 					layout="total, sizes, prev, pager, next, jumper"
 					:total="pagination.total"
@@ -74,7 +76,7 @@ export default {
 			pagination: {
 				currentPage: 1,
 				pageSize: 1,
-				total: 100,
+				total: 0,
 			},
 			list: [],
 			form: {
@@ -82,8 +84,8 @@ export default {
 				name: "", //  	否	string	学科名称
 				username: "", //  否	string	创建者用户名
 				status: "1", //  	否	string	状态 0(禁用) 1(启用)
-				// page: "", //  	否	string	页码 默认为1
-				// limit: "", //  	否	string	页尺寸 默认为10
+				page: "", //  	否	string	页码 默认为1
+				limit: "", //  	否	string	页尺寸 默认为10
 			},
 			number: 0,
 			tableData: [
@@ -114,44 +116,72 @@ export default {
 	},
 	created() {
 		// 将pagination和form的值合并
-		let _query = {
-			...this.form,
-			page: this.pagination.currentPage,
-			limit: this.pagination.pageSize,
-		};
-
-		// 进入页面获取列表数据
-		getSubjectList(_query).then((res) => {
-			console.log(res);
-			this.list = res.data.data.items;
-		});
+		this.getData();
 	},
 	mounted() {},
 	methods: {
+		// 调用接口获取列表数据
+		getData() {
+			let _query = {
+				...this.form,
+				page: this.pagination.currentPage,
+				limit: this.pagination.pageSize,
+			};
+
+			// 进入页面获取列表数据
+			getSubjectList(_query).then((res) => {
+				console.log(res);
+				this.list = res.data.data.items;
+				// 修改总页数 总页数与每一页显示的数量关联
+				this.pagination.total = res.data.data.pagination.total;
+			});
+		},
 		edit(index, row) {
 			// 获取当前的行的索引 和 当前行的所有内容
 			console.log("index", index);
 			console.log("row", row);
 		},
-		// 页码数量改变
+		// 页码数量改变 每页显示的条数
 		handleSizeChange(size) {
-			console.log("当前页容量", size);
+			// console.log("当前页容量", size);
+			this.pagination.pageSize = size;
+			// 改变每页显示的总条数的时候 如果不将其回到首页
+			// 容易出现没有数据的情况
+			this.pagination.currentPage = 1;
+			this.getData();
 		},
-		// 页码改变
+		// 页码改变 切换页码
 		handleCurrentChange(page) {
-			console.log("页码改变", page);
+			// console.log("页码改变", page);
 			this.pagination.currentPage = page;
-			console.log(this.pagination.currentPage);
+			// console.log(this.pagination.currentPage);
+			this.getData();
 		},
 	},
 };
 </script>
 
 <style lang="less">
-.card {
-	.el-input__inner {
+.subject {
+	.w100 {
 		width: 100px;
 	}
+	.w149 {
+		width: 145px;
+	}
+	.el-button {
+		padding: 12px;
+	}
+	.el-button.search,
+	.el-button.clear {
+		width: 55px;
+	}
+	.el-button.add {
+		width: 80px;
+	}
+}
+.el-form-item__content {
+	margin-left: 0;
 }
 .table-card {
 	margin-top: 20px;
@@ -162,5 +192,8 @@ export default {
 }
 .el-table .cell {
 	text-align: center;
+}
+.red {
+	color: red;
 }
 </style>
